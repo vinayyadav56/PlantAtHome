da5835335ecefed169e357235e2af61784b6043e
Vinay Yadav <yadavvinay9996@gmail.com>
2026-05-30 12:47:40 +0000

    fix: guard helper functions against redeclaration
    ShopServiceProvider::loadHelpers() uses bare require (not require_once),
    causing PHP Fatal when composer --optimize-autoloader double-loads files.
    Wrapping all three functions in if (!function_exists()) prevents the crash
    that was breaking php artisan route:cache.

--- packages/marvel/src/Helpers/ResourceHelpers.php
+++ packages/marvel/src/Helpers/ResourceHelpers.php
@@ -1,60 +1,64 @@
 <?php
 
-
-
-function getResourceData($data, array $extra = [])
-{
-    $item = [
-        'id'   => $data?->id,
-        'name' => $data?->name,
-        'slug' => $data?->slug,
-        'logo' => $data?->logo
-    ];
-
-    if (!empty($extra)) {
-        foreach ($extra as $key) {
-            if (isset($key)) {
-                $item[$key] = $data?->$key;
-            }
-        }
-    }
-    return $item;
-}
-
-function getResourceCollection($data, array $extra = [])
-{
-    return collect($data)->map(function ($item) use ($extra) {
+if (!function_exists('getResourceData')) {
+    function getResourceData($data, array $extra = [])
+    {
         $item = [
-            'id'   => $item['id'],
-            'name' => $item['name'],
-            'slug' => $item['slug'],
+            'id'   => $data?->id,
+            'name' => $data?->name,
+            'slug' => $data?->slug,
+            'logo' => $data?->logo
         ];
 
         if (!empty($extra)) {
             foreach ($extra as $key) {
-                if (isset($item[$key])) {
-                    $item[$key] = $item[$key];
+                if (isset($key)) {
+                    $item[$key] = $data?->$key;
                 }
             }
         }
         return $item;
-    })->toArray();
+    }
 }
 
-function getVariations($data)
-{
-    $variations = [];
-    foreach ($data as $item) {
-        $variations[] = [
-            'id'           => $item['id'],
-            'slug'         => $item['slug'],
-            'attribute_id' => $item['attribute_id'],
-            'value'        => $item['value'],
-            'language'     => $item['language'],
-            'meta'         => $item['meta'],
-            'translated_languages' => $item['translated_languages'],
-            'attribute' => $item['attribute'],
-        ];
+if (!function_exists('getResourceCollection')) {
+    function getResourceCollection($data, array $extra = [])
+    {
+        return collect($data)->map(function ($item) use ($extra) {
+            $item = [
+                'id'   => $item['id'],
+                'name' => $item['name'],
+                'slug' => $item['slug'],
+            ];
+
+            if (!empty($extra)) {
+                foreach ($extra as $key) {
+                    if (isset($item[$key])) {
+                        $item[$key] = $item[$key];
+                    }
+                }
+            }
+            return $item;
+        })->toArray();
     }
-    return $variations;
+}
+
+if (!function_exists('getVariations')) {
+    function getVariations($data)
+    {
+        $variations = [];
+        foreach ($data as $item) {
+            $variations[] = [
+                'id'           => $item['id'],
+                'slug'         => $item['slug'],
+                'attribute_id' => $item['attribute_id'],
+                'value'        => $item['value'],
+                'language'     => $item['language'],
+                'meta'         => $item['meta'],
+                'translated_languages' => $item['translated_languages'],
+                'attribute' => $item['attribute'],
+            ];
+        }
+        return $variations;
+    }
 }
